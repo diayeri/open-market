5a856357bbf5b971cc90f5492f7a8fdceef2f88e
dia <jdyoung1031@gmail.com>
2024-08-05 16:02:05 +0900

    로그인 - 계정 판별

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,25 +1,53 @@
-// - 경고문구: 입력하지 않은채 누를때
+const url = "https://openmarket.weniv.co.kr";
+const fetchHeaders = { "Content-Type": "application/json" };
+
 const $buyerLoginForm = document.querySelector("#buyerLoginForm");
 const $buyerLoginBtn = document.querySelector("#buyerLoginBtn");
 const $buyerId = document.querySelector("#buyerId");
 const $buyerPassword = document.querySelector("#buyerPassword");
 const $buyerLoginErr = document.querySelector("#buyerLoginErr");
 
-$buyerLoginForm.addEventListener("submit", (e) => {
+// 아이디 데이터 가져오기
+const login = async (id, pw, type) => {
+  try {
+    const res = await fetch(url + "/accounts/login/", {
+      method: "POST",
+      headers: fetchHeaders,
+      body: JSON.stringify({
+        username: id,
+        password: pw,
+        login_type: type,
+      }),
+    });
+    const data = await res.json();
+    return { data: data, ok: res.ok };
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+$buyerLoginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  if ($buyerId.value.trim() && $buyerPassword.value) {
-    // 로그인 데이터 비교
-    // 불일치시, 경고문구 보이기
-    // 비밀번호 입력창 포커싱, 값 초기화
+  const id = $buyerId.value.trim();
+  const pw = $buyerPassword.value;
+  if (id && pw) {
+    const loginRes = await login(id, pw, "BUYER");
+    if (loginRes.ok) {
+      // 성공 (계정정보 있음)
+      console.log("성공");
+    } else {
+      // 실패
+      console.log("실패");
+    }
   } else {
     // 미입력시, 경고문구 보이기
     $buyerLoginErr.classList.remove("hidden");
     // 입력되지 않은칸 포커싱
-    if (!$buyerId.value.trim()) {
+    if (!id) {
       $buyerLoginErr.textContent = "아이디를 입력해주세요.";
-      $buyerId.value = $buyerId.value.trim(); // 공백 입력시 초기화
+      $buyerId.value = id; // 공백 입력시 초기화
       $buyerId.focus();
-    } else if (!$buyerPassword.value) {
+    } else if (!pw) {
       $buyerLoginErr.textContent = "비밀번호를 입력해주세요.";
       $buyerPassword.focus();
     }
