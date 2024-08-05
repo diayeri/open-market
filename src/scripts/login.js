db952a8b64225c67a9b838ac3bb18c9885edbb18
dia <jdyoung1031@gmail.com>
2024-08-05 17:02:50 +0900

    로그인 - 탭 전환

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -2,10 +2,14 @@ const url = "https://openmarket.weniv.co.kr";
 const fetchHeaders = { "Content-Type": "application/json" };
 
 const $buyerLoginForm = document.querySelector("#buyerLoginForm");
+const $buyerLoginTab = document.querySelector("#buyerLoginTab");
 const $buyerLoginBtn = document.querySelector("#buyerLoginBtn");
 const $buyerId = document.querySelector("#buyerId");
 const $buyerPassword = document.querySelector("#buyerPassword");
 const $buyerLoginErr = document.querySelector("#buyerLoginErr");
+
+const $sellerLoginForm = document.querySelector("#sellerLoginForm");
+const $sellerLoginTab = document.querySelector("#sellerLoginTab");
 
 const login = async (id, pw, type) => {
   try {
@@ -57,4 +61,20 @@ $buyerLoginForm.addEventListener("submit", async (e) => {
       $buyerPassword.focus();
     }
   }
+});
+
+// 로그인 탭 전환
+function toggleTabs(isBuyer) {
+  $buyerLoginTab.classList.toggle("active", isBuyer);
+  $sellerLoginTab.classList.toggle("active", !isBuyer);
+  $buyerLoginForm.classList.toggle("hidden", !isBuyer);
+  $sellerLoginForm.classList.toggle("hidden", isBuyer);
+}
+
+$buyerLoginTab.addEventListener("click", function () {
+  toggleTabs(true);
+});
+
+$sellerLoginTab.addEventListener("click", function () {
+  toggleTabs(false);
 });
