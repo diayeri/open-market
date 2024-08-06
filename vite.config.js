616bd7d68dd7d17fce6b8a692dda9cfaf08e6a97
dia <jdyoung1031@gmail.com>
2024-08-06 17:13:42 +0900

    빌드 설정 변경 (MPA)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  root: "src",
-  base: "/",
+  // root: "src",
+  base: "/open-market/",
   build: {
-    outDir: "../public",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "src/index.html"),
+        login: path.resolve(__dirname, "src/login.html"),
+        cart: path.resolve(__dirname, "src/cart.html"),
+        // signup: path.resolve(__dirname, "src/signup.html"),
+        // product: path.resolve(__dirname, "src/product.html"),
+        // payment: path.resolve(__dirname, "src/payment.html"),
+        // error404: path.resolve(__dirname, "src/error404.html"),
+      },
+    },
+    outDir: "dist",
+    assetsDir: "assets",
   },
   server: {
     port: 3000,
