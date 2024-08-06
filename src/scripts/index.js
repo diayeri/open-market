c4918bc21115c0e167e30afd84c1e70874fd1823
dia <jdyoung1031@gmail.com>
2024-08-06 11:35:14 +0900

    상품목록 - 카드 데이터 불러오기

---
+++ src/scripts/index.js
@@ -0,0 +1,41 @@
+const url = "https://openmarket.weniv.co.kr";
+const fetchHeaders = { "Content-Type": "application/json" };
+
+// 상품목록 UI
+const addListUi = (e) => {
+  // const $li = document.createElement("li");
+  // $li.classList.add("product-list");
+  e.price = e.price.toLocaleString();
+
+  const $ul = document.querySelector(".product-list");
+  const $li = `
+    <li>
+      <a href="/${e.product_id}">
+        <img
+          src="${e.image}"
+          alt="${e.product_name}"
+        />
+        <div class="wrap-text">
+          <p class="subt">${e.store_name}</p>
+          <h3 class="title">${e.product_name}</h3>
+          <div class="price">
+            <p>${e.price}</p>
+            <span>원</span>
+          </div>
+        </div>
+      </a>
+    </li>
+  `;
+  $ul.insertAdjacentHTML("beforeend", $li);
+};
+
+// 상품목록 불러오기
+const loadProducts = async () => {
+  const res = await fetch(url + "/products");
+  const products = await res.json();
+  console.log(products.results[0]);
+  products.results.forEach((e) => {
+    addListUi(e);
+  });
+};
+loadProducts();
