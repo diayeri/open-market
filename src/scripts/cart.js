64b7c1176d5143234351b7c3fe5508b73d027a43
dia <jdyoung1031@gmail.com>
2024-08-06 16:32:00 +0900

    팝업창 가려두기

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -3,7 +3,6 @@ const dialog = document.getElementById("checkModal");
 // const showDialogBtn = document.getElementById("showDialogBtn");
 const closeBtn = document.getElementById("closeBtn");
 
-dialog.showModal();
 // showDialogBtn.addEventListener("click", () => {
 //   dialog.showModal();
 //   console.log("오르미 FE 짱짱");
