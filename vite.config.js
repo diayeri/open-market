905ac03fab25ab54f896606ee5ccb697f585790e
dia <jdyoung1031@gmail.com>
2024-08-06 18:04:08 +0900

    리팩토링 - 빌드관련 불필요한 세팅 제거 - html 파일위치 변경

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
 import { defineConfig } from "vite";
-import path from "path";
 
 export default defineConfig({
-  // root: "src",
-  base: "/open-market/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "src/index.html"),
-        login: path.resolve(__dirname, "src/login.html"),
-        cart: path.resolve(__dirname, "src/cart.html"),
-        // signup: path.resolve(__dirname, "src/signup.html"),
-        // product: path.resolve(__dirname, "src/product.html"),
-        // payment: path.resolve(__dirname, "src/payment.html"),
-        // error404: path.resolve(__dirname, "src/error404.html"),
-      },
-    },
-    outDir: "dist",
-    assetsDir: "assets",
-  },
+  // base: "/open-market/",
   server: {
     port: 3000,
   },
