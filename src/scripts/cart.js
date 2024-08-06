2dc74976e3643abcc55eae721f559e9b86ca7ea4
dia <jdyoung1031@gmail.com>
2024-08-06 16:26:29 +0900

    장바구니 팝업창 ui

---
+++ src/scripts/cart.js
@@ -0,0 +1,14 @@
+// 모달 기능
+const dialog = document.getElementById("checkModal");
+// const showDialogBtn = document.getElementById("showDialogBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+dialog.showModal();
+// showDialogBtn.addEventListener("click", () => {
+//   dialog.showModal();
+//   console.log("오르미 FE 짱짱");
+// });
+
+closeBtn.addEventListener("click", () => {
+  dialog.close();
+});
